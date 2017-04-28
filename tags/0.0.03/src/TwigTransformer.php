459e0f4c3e976c660d79c7e63b53f07daca10664
chris <chris@09f6963d-2619-41be-b46b-60155823d8f4>
2017-04-28 11:22:02 +0000

    Tagged 0.0.03

---
+++ tags/0.0.03/src/TwigTransformer.php
@@ -0,0 +1,42 @@
+<?php
+namespace TrunkSoftware\Component\TwigTransformer;
+
+use Trunk\EmailLibrary\EMail\transformer\BaseTransformer;
+use Trunk\Wibbler\WibblerDependencyContainer;
+use Trunk\EmailLibrary\EMail\Message;
+
+class TwigTransformer extends BaseTransformer {
+
+	/**
+	 * @var WibblerDependencyContainer
+	 */
+	private $dependency_manager;
+
+	/**
+	 * @var \Trunk\Wibbler\Modules\twig
+	 */
+	private $twig;
+
+	/**
+	 * @var string
+	 */
+	private $template_location = "";
+
+	public function __construct( $template_location ) {
+		$this->template_location = $template_location;
+
+		$this->dependency_manager = WibblerDependencyContainer::Instance();
+		$this->twig = $this->dependency_manager->getService( "twig" );
+	}
+
+	/**
+	 * @param Message $message
+	 */
+	public function transform( Message &$message ) {
+
+		$params = $message->getParams();
+		$params = array_merge( $params, [ 'body' => $message->getBody() ] );
+		$message->setBody( $this->twig->render( $this->template_location, $params ) );
+	}
+
+}
